2a0ab9fdd9d2f5bc3e15e8c3645570a17f251636
David <Phireh@hotmail.com>
2021-05-21 19:07:30 +0200

    Day 11, part 2

--- day-11/day-11.lisp
+++ day-11/day-11.lisp
@@ -57,6 +57,90 @@
       (push (seat (1+ i) (1+ j)) adj-seat-list))
     adj-seat-list))
 
+(defun adjacent-seats-part-2 (i j)
+  (let ((adj-seat-list '()))
+    ;; Up+left direction
+    (loop :for row = (1- i) :then (1- row)
+          :for col = (1- j) :then (1- col)
+          :if (or (< row 0) (< col 0)) :do
+            (return nil)
+          :else :do
+            (when (char/= (seat row col) #\.)
+              (push (seat row col) adj-seat-list)
+              (return)))
+    
+    ;; Up direction
+    (loop :for row = (1- i) :then (1- row)
+          :with col = j
+          :if (< row 0) :do
+            (return nil)
+          :else :do
+            (when (char/= (seat row col) #\.)
+              (push (seat row col) adj-seat-list)
+              (return)))
+
+    ;; Up+right direction
+    (loop :for row = (1- i) :then (1- row)
+          :for col = (1+ j) :then (1+ col)
+          :if (or (< row 0) (> col (1- line-len))) :do
+            (return nil)
+          :else :do
+            (when (char/= (seat row col) #\.)
+              (push (seat row col) adj-seat-list)
+              (return)))
+
+    ;; Left direction
+    (loop :with row = i
+          :for col = (1- j) :then (1- col)
+          :if (< col 0) :do
+            (return nil)
+          :else :do
+            (when (char/= (seat row col) #\.)
+              (push (seat row col) adj-seat-list)
+              (return)))
+
+    ;; Right direction
+    (loop :with row = i
+          :for col = (1+ j) :then (1+ col)
+          :if (> col (1- line-len)) :do
+            (return nil)
+          :else :do
+            (when (char/= (seat row col) #\.)
+              (push (seat row col) adj-seat-list)
+              (return)))
+
+    ;; Down+left direction
+    (loop :for row = (1+ i) :then (1+ row)
+          :for col = (1- j) :then (1- col)
+          :if (or (> row (1- line-count)) (< col 0)) :do
+            (return nil)
+          :else :do
+            (when (char/= (seat row col) #\.)
+              (push (seat row col) adj-seat-list)
+              (return)))
+
+    ;; Down direction
+    (loop :for row = (1+ i) :then (1+ row)
+          :with col = j
+          :if (> row (1- line-count)) :do
+            (return nil)
+          :else :do
+            (when (char/= (seat row col) #\.)
+              (push (seat row col) adj-seat-list)
+              (return)))
+
+    ;; Down+right direction
+    (loop :for row = (1+ i) :then (1+ row)
+          :for col = (1+ j) :then (1+ col)
+          :if (or (> row (1- line-count)) (> col (1- line-len))) :do
+            (return nil)
+          :else :do
+            (when (char/= (seat row col) #\.)
+              (push (seat row col) adj-seat-list)
+              (return)))    
+    
+    adj-seat-list))
+
 ;; Returns a new iteration of the seats
 (defun iterate-seats ()
   (let ((new-iteration (make-array (list line-count line-len) :element-type 'character)))
@@ -72,6 +156,29 @@
                          (setf new-c #\L))
                    :when (char= c #\#) :do
                      (if (>= (count #\# adj-seats) 4)
+                         (setf new-c #\L)
+                         (setf new-c #\#))
+                   :when (char= c #\.) :do
+                     (setf new-c #\.)
+                   :do
+                      (setf (row-major-aref new-iteration (array-row-major-index new-iteration i j)) new-c))
+          :finally (return new-iteration))))
+
+;; Returns a new iteration of the seats
+(defun iterate-seats-part-2 ()
+  (let ((new-iteration (make-array (list line-count line-len) :element-type 'character)))
+    (loop :for i :from 0 :to (1- line-count)
+          :do
+             (loop :for j :from 0 :to (1- line-len)
+                   :for adj-seats = (adjacent-seats-part-2 i j)
+                   :for c = (seat i j)
+                   :with new-c = #\.
+                   :when (char= c #\L) :do
+                     (if (= (count #\# adj-seats) 0)
+                         (setf new-c #\#)
+                         (setf new-c #\L))
+                   :when (char= c #\#) :do
+                     (if (>= (count #\# adj-seats) 5)
                          (setf new-c #\L)
                          (setf new-c #\#))
                    :when (char= c #\.) :do
@@ -108,3 +215,12 @@
         :else :do
            (setf seat-arr new-iteration)))
 
+(defun part-2 ()
+  (make-seat-array)
+  (loop :for i = 1 :then (1+ i)
+        :for new-iteration = (iterate-seats-part-2)
+        :if (seat-array= seat-arr new-iteration) :do
+          (setf seat-arr new-iteration)          
+          (return (values i (occupied-seats)))
+        :else :do
+          (setf seat-arr new-iteration)))
